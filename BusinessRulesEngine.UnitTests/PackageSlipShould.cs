c0e5a3304c3e720d013099afbff61748e6be99c9
Ritu Khilrani <Ritu.Khilrani@asos.com>
2020-09-07 01:18:07 +0530

    test case fo video updated

--- BusinessRulesEngine.UnitTests/PackageSlipShould.cs
+++ BusinessRulesEngine.UnitTests/PackageSlipShould.cs
@@ -20,5 +20,13 @@
             var packageSlip = PackageSlip.GeneratePackagingSlip(inputOrder.ToUpper());
             packageSlip.Should().Be("Packaging slip successfully generated for royalty department");
         }
+
+        [Fact]
+        public void When_book_is_received_Generate_PackagingSlip_and_add_free_video_Is_Successful()
+        {
+            const string inputOrder = "video";
+            var packageSlip = PackageSlip.GeneratePackagingSlip(inputOrder.ToUpper());
+            packageSlip.Should().Be("Packaging slip generated and first aid video added");
+        }
     }
 }
