99e51044081941da5fad12bf83614d5eabb2feb5
Ritu Khilrani <Ritu.Khilrani@asos.com>
2020-09-06 23:49:52 +0530

    adding test and base project for 1st requirement

---
+++ BusinessRulesEngine.UnitTests/PackageSlipShould.cs
@@ -0,0 +1,16 @@
+﻿namespace BusinessRulesEngine.UnitTests
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class PackageSlipShould
+    {
+        [Fact]
+        public void When_physical_product_is_received_Generate_packagingSlip_Is_Successful()
+        {
+            const string inputOrder = "pen";
+            var packageSlip = PackageSlip.GeneratePackagingSlip(inputOrder);
+            packageSlip.Should().Be("Packaging slip successfully generated for shipping");
+        }
+    }
+}
---
+++ BusinessRulesEngine/PackageSlip.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessRulesEngine
+{
+    public class PackageSlip
+    {
+        public static string GeneratePackagingSlip(string inputOrder)
+        {
+            var successfulMessage = "Packaging slip successfully generated for shipping";
+            return successfulMessage;
+        }
+    }
+}
