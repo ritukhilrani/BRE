5d7f9b054be1d57a2a42c8080e69f75fca56ba36
Ritu Khilrani <Ritu.Khilrani@asos.com>
2020-09-07 01:14:19 +0530

    changes for final requirement

--- BusinessRulesEngine.UnitTests/PackageSlipShould.cs
+++ BusinessRulesEngine.UnitTests/PackageSlipShould.cs
@@ -8,8 +8,8 @@
         [Fact]
         public void When_physical_product_is_received_Generate_packagingSlip_Is_Successful()
         {
-            const string inputOrder = "pen";
-            var packageSlip = PackageSlip.GeneratePackagingSlip(inputOrder);
+            const string inputOrder = "physicalproduct";
+            var packageSlip = PackageSlip.GeneratePackagingSlip(inputOrder.ToUpper());
             packageSlip.Should().Be("Packaging slip successfully generated for shipping");
         }
 
@@ -17,7 +17,7 @@
         public void When_book_is_received_Generate_duplicatePackagingSlip_Is_Successful()
         {
             const string inputOrder = "book";
-            var packageSlip = PackageSlip.GenerateDuplicatePackagingSlip(inputOrder);
+            var packageSlip = PackageSlip.GeneratePackagingSlip(inputOrder.ToUpper());
             packageSlip.Should().Be("Packaging slip successfully generated for royalty department");
         }
     }
--- BusinessRulesEngine/PackageSlip.cs
+++ BusinessRulesEngine/PackageSlip.cs
@@ -1,17 +1,39 @@
 ﻿namespace BusinessRulesEngine
 {
+    using System;
+
     public class PackageSlip
     {
         private static string successfulMessage;
         public static string GeneratePackagingSlip(string inputOrder)
         {
-            successfulMessage = "Packaging slip successfully generated for shipping";
+            switch (inputOrder)
+            {
+                case "PHYSICALPRODUCT":
+                    successfulMessage = GeneratePackagingSlipForProductOrdered("shipping");
+                    break;
+                case "BOOK":
+                    successfulMessage = GeneratePackagingSlipForProductOrdered("royalty department");
+                    break;
+                case "VIDEO":
+                    successfulMessage = GeneratePackagingSlipForProductOrdered("C://FilePath/ToVideo");
+                    break;
+                default:
+                    Console.WriteLine("Invalid entry");
+                    break;
+            }
+            
+            Console.WriteLine(successfulMessage);
             return successfulMessage;
         }
 
-        public static string GenerateDuplicatePackagingSlip(string inputOrder)
+        private static string GeneratePackagingSlipForProductOrdered(string inputOrder)
         {
-            successfulMessage = "Packaging slip successfully generated for royalty department";
+            if (System.IO.Directory.Exists(inputOrder))
+            {
+                Console.WriteLine("Packaging slip generated and first aid video added");
+            }
+            successfulMessage = "Packaging slip successfully generated for " + inputOrder;
             return successfulMessage;
         }
     }
